1360c9397a5a44cd1205b57b2b66e9780d83a214
Sebastian Finkel <s.finkel@posteo.de>
2022-11-25 23:14:39 +0100

    lesson 1 setup

---
+++ lesson_1.js
@@ -0,0 +1,16 @@
+// Hilfreiche Tastenkombinationen fuer IntelliJ
+
+//  1.) Vorschlaege anzeigen: ALT + ENTER
+
+function changeMe (type) {
+    let innerType = type
+    return innerType.toUpperCase()
+}
+
+// 2.) Variablen umbennen: SHIFT-F6
+
+changeMe('test')
+
+// you cannot changeMe
+
+// Zeilen aus- und einkommentieren: ALT + /
